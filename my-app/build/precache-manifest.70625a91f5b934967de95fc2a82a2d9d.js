4df7cbda45dc14c41c7a56f1726094e6dc557c13
ClayGao <r700000@gmail.com>
2019-09-26 23:30:23 +0800

    week22

---
+++ my-app/build/precache-manifest.70625a91f5b934967de95fc2a82a2d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2024479a7808232f6754423b878a2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d77adf49b0d2b012a9b0",
+    "url": "./static/css/main.8944381c.chunk.css"
+  },
+  {
+    "revision": "132e6a7f5e9f8752d725",
+    "url": "./static/js/2.d60f10f8.chunk.js"
+  },
+  {
+    "revision": "d77adf49b0d2b012a9b0",
+    "url": "./static/js/main.a7bec592.chunk.js"
+  },
+  {
+    "revision": "f49e0e74df6e6c3c28bc",
+    "url": "./static/js/runtime-main.04a13e19.js"
+  },
+  {
+    "revision": "54e40ca1322beb42e001bce85f2e9058",
+    "url": "./static/media/bg.54e40ca1.jpg"
+  }
+]);
